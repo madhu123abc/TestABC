0446e191d7602f9d2b2d5ed247f7593660b04aa0
Madhavi <111486548+madhu123abc@users.noreply.github.com>
2022-10-13 22:23:14 -0600

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+package com.Assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		int temp;
+		int a=10; 
+		int b =20;
+		
+		 temp =a;
+		  a=b;
+		  b=temp;
+    System.out.println("Number a="    +a);
+    System.out.println("Number b = "       +b);
+    
+	}
+
+}
